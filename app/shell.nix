d3dc18df737ba92ccfd3b0ebd64d8aa90113ff2c
Bobbe <maxschlecht@web.de>
2024-09-16 15:54:26 +0200

    Add shell.nix for flutter app

---
+++ app/shell.nix
@@ -0,0 +1,27 @@
+{
+    pkgs ?
+        import <nixpkgs> {
+            config = {
+                android_sdk.accept_license = true;
+                allowUnfree = true;
+            };
+        },
+    androidPkgs ? (
+        pkgs.androidenv.composeAndroidPackages
+        {
+            buildToolsVersions = ["34.0.0" "30.0.3"];
+            platformVersions = ["34"];
+            abiVersions = ["armeabi-v7a" "arm64-v8a" "x86_64"];
+            includeSystemImages = true;
+            systemImageTypes = ["google_apis"];
+        }
+    ),
+}:
+pkgs.mkShell {
+    ANDROID_SDK_ROOT = "${androidPkgs.androidsdk}/libexec/android-sdk";
+    nativeBuildInputs = with pkgs.buildPackages; [
+        androidPkgs.androidsdk
+        flutter322
+        jdk19
+    ];
+}
