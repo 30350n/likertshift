9aba4f6816f40dcb7306c77716ede3de75f1c999
Max Schlecht <maxschlecht@web.de>
2025-01-25 18:24:26 +0100

    Refactor plot script

--- evaluation/plot.py
+++ evaluation/plot.py
@@ -97,121 +97,125 @@ def test_math():
 ROUTE = "south_route"
 METHOD = "*"
 
-folium_map = folium.Map(max_zoom=24)
 
-for i, path in enumerate(PARENT_DIRECTORY.glob(f"data/*/recordings/*/rec-*-{METHOD}-{ROUTE}.csv")):
-    with path.open(encoding="utf-8") as file:
-        reader = csv.reader(file)
-        next(reader)
-        data = np.array([[float(v) if v.strip() else 0.0 for v in row] for row in reader])
-        data = np.hstack((data, np.arange(data.shape[0])[:, np.newaxis]))
+def plot_test_map():
+    folium_map = folium.Map(max_zoom=24)
 
-    participant_id = path.parents[2].stem
-    recording_id, method = path.stem.split("-")[1:3]
-
-    if (filter_path := path.with_stem("filter")).is_file():
-        with filter_path.open(encoding="utf-8") as file:
+    for path in PARENT_DIRECTORY.glob(f"data/*/recordings/*/rec-*-{METHOD}-{ROUTE}.csv"):
+        with path.open(encoding="utf-8") as file:
             reader = csv.reader(file)
             next(reader)
-            sections = [(int(row[0]), int(row[1])) for row in reader]
+            data = np.array([[float(v) if v.strip() else 0.0 for v in row] for row in reader])
+            data = np.hstack((data, np.arange(data.shape[0])[:, np.newaxis]))
 
-        data = np.concatenate([data[np.arange(start, end + 1)] for start, end in sections])
-    else:
-        print(f"missing filter for {path.name})")
+        participant_id = path.parents[2].stem
+        recording_id, method = path.stem.split("-")[1:3]
 
-    coordinates = data[:, 0:2]
-    times = data[:, 2]
-    ratings = data[:, 3]
-    indices = data[:, 4].astype(int)
+        if (filter_path := path.with_stem("filter")).is_file():
+            with filter_path.open(encoding="utf-8") as file:
+                reader = csv.reader(file)
+                next(reader)
+                sections = [(int(row[0]), int(row[1])) for row in reader]
 
-    if np.all(ratings == ratings[0]):
-        pass  # continue
+            data = np.concatenate([data[np.arange(start, end + 1)] for start, end in sections])
+        else:
+            print(f"missing filter for {path.name})")
 
-    feature_group = folium.FeatureGroup(
-        f"{method} {participant_id} ({recording_id})",
-    ).add_to(folium_map)
+        coordinates = data[:, 0:2]
+        ratings = data[:, 3]
+        indices = data[:, 4].astype(int)
+
+        if np.all(ratings == ratings[0]):
+            pass  # continue
+
+        feature_group = folium.FeatureGroup(
+            f"{method} {participant_id} ({recording_id})",
+        ).add_to(folium_map)
+
+        folium.ColorLine(
+            coordinates,
+            colors=ratings,
+            colormap=COLORMAP,
+            weight=5,
+        ).add_to(feature_group)
+
+        coordinate: tuple[float, float]
+        index: int
+        rating: float
+        for index, coordinate, rating in zip(indices, coordinates, ratings):
+            folium.CircleMarker(
+                coordinate,
+                radius=3,
+                fill=True,
+                color=COLORMAP(rating),
+                tooltip=f"{index} ({rating}, {participant_id} {recording_id})",
+            ).add_to(feature_group)
+
+    with (PARENT_DIRECTORY / "routes" / f"{ROUTE}.json").open(encoding="utf-8") as file:
+        json_data: dict[str, Any] = json.load(file)
+
+    route_coordinates: list[tuple[float, float]] = json_data["coordinates"]
+    route_road_types: list[str] = json_data["types"]
+    unique_road_types: list[str] = np.unique(route_road_types).tolist()
+
+    print("coordinates:", len(route_coordinates), "; route types:", len(route_road_types))
+    total_distance = 0.0
+    distance_per_road_type = {road_type: 0.0 for road_type in unique_road_types}
+    for p1, p2, road_type in zip(route_coordinates[:-1], route_coordinates[1:], route_road_types):
+        distance = haversine_distance(np.radians(p1), np.radians(p2)) * EARTH_RADIUS
+        total_distance += distance
+        distance_per_road_type[road_type] += distance
+
+    print(f"total distance    : {total_distance:4.0f}m")
+    for road_type, distance in distance_per_road_type.items():
+        print(f"{road_type.ljust(18)}: {distance:4.0f}m")
+    print("n crossings       :   ", route_road_types.count("crossing"))
+
+    SET1 = colormaps["Set1"]
+
+    OTHER_COLOR = cast(TypeAnyColorType, SET1.colors[7])  # pyright: ignore[reportAttributeAccessIssue]
+    COLOR_MAPPING = cast(
+        dict[str, TypeAnyColorType],
+        {
+            "road": SET1.colors[1],  # pyright: ignore[reportAttributeAccessIssue]
+            "bike_path": SET1.colors[4],  # pyright: ignore[reportAttributeAccessIssue]
+            "pedestrian_way": SET1.colors[2],  # pyright: ignore[reportAttributeAccessIssue]
+            "mixed_path": SET1.colors[3],  # pyright: ignore[reportAttributeAccessIssue]
+            "crossing": SET1.colors[0],  # pyright: ignore[reportAttributeAccessIssue]
+        },
+    )
+
+    road_type_colormap = colormap.StepColormap(
+        [COLOR_MAPPING.get(road_type, OTHER_COLOR) for road_type in unique_road_types]
+    )
+    colors = [
+        (unique_road_types.index(road_type) + 0.5) / len(unique_road_types)
+        for road_type in route_road_types
+    ]
+
+    feature_group = folium.FeatureGroup("reference", show=False).add_to(folium_map)
 
     folium.ColorLine(
-        coordinates,
-        colors=ratings,
-        colormap=COLORMAP,
-        weight=5,
+        route_coordinates,
+        colors,
+        colormap=road_type_colormap,
+        weight=10,
     ).add_to(feature_group)
 
-    coordinate: tuple[float, float]
-    index: int
-    rating: float
-    for index, coordinate, rating in zip(indices, coordinates, ratings):
+    route_types2 = route_road_types + route_road_types[-1:]
+    for index, (coordinate, road_type) in enumerate(zip(route_coordinates, route_types2)):
         folium.CircleMarker(
             coordinate,
             radius=3,
             fill=True,
-            color=COLORMAP(rating),
-            tooltip=f"{index} ({rating}, {participant_id} {recording_id})",
+            color=COLOR_MAPPING.get(road_type, OTHER_COLOR),  # pyright: ignore[reportArgumentType]
+            tooltip=f"{index}",
         ).add_to(feature_group)
 
-with (PARENT_DIRECTORY / "routes" / f"{ROUTE}.json").open(encoding="utf-8") as file:
-    json_data: dict[str, Any] = json.load(file)
+    folium.LayerControl().add_to(folium_map)
+    folium_map.fit_bounds(folium_map.get_bounds())  # pyright: ignore[reportArgumentType]
+    folium_map.add_child(folium.LatLngPopup())
+    folium_map.show_in_browser()
 
-route_coordinates: list[tuple[float, float]] = json_data["coordinates"]
-route_road_types: list[str] = json_data["types"]
-unique_road_types: list[str] = np.unique(route_road_types).tolist()
 
-print("coordinates:", len(route_coordinates), "; route types:", len(route_road_types))
-total_distance = 0.0
-distance_per_road_type = {road_type: 0.0 for road_type in unique_road_types}
-for p1, p2, road_type in zip(route_coordinates[:-1], route_coordinates[1:], route_road_types):
-    distance = haversine_distance(np.radians(p1), np.radians(p2)) * EARTH_RADIUS
-    total_distance += distance
-    distance_per_road_type[road_type] += distance
-
-print(f"total distance    : {total_distance:4.0f}m")
-for road_type, distance in distance_per_road_type.items():
-    print(f"{road_type.ljust(18)}: {distance:4.0f}m")
-print("n crossings       :   ", route_road_types.count("crossing"))
-
-SET1 = colormaps["Set1"]
-
-OTHER_COLOR = cast(TypeAnyColorType, SET1.colors[7])  # pyright: ignore[reportAttributeAccessIssue]
-COLOR_MAPPING = cast(
-    dict[str, TypeAnyColorType],
-    {
-        "road": SET1.colors[1],  # pyright: ignore[reportAttributeAccessIssue]
-        "bike_path": SET1.colors[4],  # pyright: ignore[reportAttributeAccessIssue]
-        "pedestrian_way": SET1.colors[2],  # pyright: ignore[reportAttributeAccessIssue]
-        "mixed_path": SET1.colors[3],  # pyright: ignore[reportAttributeAccessIssue]
-        "crossing": SET1.colors[0],  # pyright: ignore[reportAttributeAccessIssue]
-    },
-)
-
-road_type_colormap = colormap.StepColormap(
-    [COLOR_MAPPING.get(road_type, OTHER_COLOR) for road_type in unique_road_types]
-)
-colors = [
-    (unique_road_types.index(road_type) + 0.5) / len(unique_road_types)
-    for road_type in route_road_types
-]
-
-feature_group = folium.FeatureGroup("reference", show=False).add_to(folium_map)
-
-folium.ColorLine(
-    route_coordinates,
-    colors,
-    colormap=road_type_colormap,
-    weight=10,
-).add_to(feature_group)
-
-route_types2 = route_road_types + route_road_types[-1:]
-for index, (coordinate, road_type) in enumerate(zip(route_coordinates, route_types2)):
-    folium.CircleMarker(
-        coordinate,
-        radius=3,
-        fill=True,
-        color=COLOR_MAPPING.get(road_type, OTHER_COLOR),  # pyright: ignore[reportArgumentType]
-        tooltip=f"{index}",
-    ).add_to(feature_group)
-
-folium.LayerControl().add_to(folium_map)
-folium_map.fit_bounds(folium_map.get_bounds())  # pyright: ignore[reportArgumentType]
-folium_map.add_child(folium.LatLngPopup())
-folium_map.show_in_browser()
+plot_test_map()
